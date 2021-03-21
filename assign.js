9627295306784d4366dd73057f86262970288ce1
Sharonoito <oitosharonakoth@gmail.com>
2021-03-21 21:02:25 +0300

    second commit

--- assign.js
+++ assign.js
@@ -10,3 +10,36 @@ var fruit={
 
 fruit.fruitCost(30,2);
 console.log(fruit.quantity  + " " + fruit.name + " " +  "for KES " + totalCost.toFixed(2));
+
+
+// Examples showing difference in function,object and method
+
+function name(){
+    var name="Sharon";
+    console.log(name);
+    
+}
+name();
+
+var plant={
+    name:"Sunflower",
+    leaves:"Green",
+    flower:"yellow",   
+
+}
+console.log(plant.flower);
+
+var house={
+    roof:"Ironsheet",
+    door:"mettalic",
+    windows:4,
+    price:500,
+    // total cost of the windows
+    windowsCost:function (windows,price) {
+        totalCost=price*windows;
+       return this.windowsCost=totalCost}
+        
+    }
+    
+house.windowsCost(500,4);
+console.log(" A " + ""+ "house made of  " +house.roof +" " +  "roof" + "" +  "and" + " ",house.door +" "+ "door"   )
